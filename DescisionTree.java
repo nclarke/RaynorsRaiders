8bd7eb5605918c874aa73305feb11e27a6d5e30e
nclarke <nclarke@calpoly.edu>
2013-11-17 13:34:43 -0800

    Descision Tree

--- DescisionTree.java
+++ DescisionTree.java
@@ -21,20 +21,61 @@ public class DescisionTree {
 	}
 
 	
-	private boolean canIBuildThis(Unit unit)
+	public UnitTypes canIBuildThis(Unit unit)
 	{
 		return canIBuildThis(unit.getTypeID());
 	}
 	
-	private boolean canIBuildThis(int unitType)
+	//return null if dont need to build anythign else
+	public UnitTypes canIBuildThis(int unitType)
 	{
-		if (unitType == UnitTypes.Terran_Marine.ordinal())
+		if ((unitType == UnitTypes.Terran_Marine.ordinal()) || (unitType == UnitTypes.Terran_Firebat.ordinal()))
+		{
 			for (Unit building : this.mBld.builtBuildings)		
 				if (building.getID() == UnitTypes.Terran_Barracks.ordinal())
-					return true;
+					return null;
+			return UnitTypes.Terran_Barracks;
+		}
+		else if (unitType == UnitTypes.Terran_Barracks.ordinal() || unitType == UnitTypes.Terran_Engineering_Bay.ordinal())
+		{
+			for (Unit building : this.mBld.builtBuildings)		
+				if (building.getID() == UnitTypes.Terran_Command_Center.ordinal())
+					return null;
+			return UnitTypes.Terran_Command_Center;
+		}
+		else if (unitType == UnitTypes.Terran_Bunker.ordinal() || unitType == UnitTypes.Terran_Academy.ordinal() || unitType == UnitTypes.Terran_Factory.ordinal())
+		{
+			for (Unit building : this.mBld.builtBuildings)		
+				if (building.getID() == UnitTypes.Terran_Barracks.ordinal())
+					return null;
+			return UnitTypes.Terran_Barracks;
+		}
+		else if (unitType == UnitTypes.Terran_Starport.ordinal() || unitType == UnitTypes.Terran_Armory.ordinal())
+		{
+			for (Unit building : this.mBld.builtBuildings)		
+				if (building.getID() == UnitTypes.Terran_Factory.ordinal())
+					return null;
+			return UnitTypes.Terran_Factory;
+		}
+		else if (unitType == UnitTypes.Terran_Science_Facility.ordinal())
+		{
+			for (Unit building : this.mBld.builtBuildings)		
+				if (building.getID() == UnitTypes.Terran_Starport.ordinal())
+					return null;
+			return UnitTypes.Terran_Starport;
+		}
 		
+/*
+		else if (unitType == UnitTypes.Terran_Ghost.ordinal())
+		{
+			for (Unit building : this.mBld.builtBuildings)		
+				if (building.getID() == UnitTypes.Terran_Factory.ordinal())
+					return null;
+			return UnitTypes.Terran_Factory;
+		}
+		*/
 
-		return false;
+		return null;
 	}
 
 }
