dd3ca89df9bab72f88cf7decc7c8d90d568b67c2
Ben <tupasben@gmail.com>
2013-11-20 17:54:11 -0800

    reworked decision tree to use new building stack

--- DescisionTree.java
+++ DescisionTree.java
@@ -8,6 +8,7 @@ import javabot.RaynorsRaiders.Level;
 import javabot.model.*;
 import javabot.types.UnitType.UnitTypes;
 import javabot.util.*;
+import javabot.RaynorsRaiders.ManagerBuild.BuildingRR;
 
 public class DescisionTree {
 
@@ -26,53 +27,79 @@ public class DescisionTree {
 		return canIBuildThis(unit.getTypeID());
 	}
 	
-	//return null if dont need to build anythign else
+	//return null if dont need to build anything else
 	public UnitTypes canIBuildThis(int unitType)
 	{
+		Unit building, building1, building2, building3;
+		
 		if (unitType == UnitTypes.Terran_Barracks.ordinal() || unitType == UnitTypes.Terran_Engineering_Bay.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
-				if (building.getID() == UnitTypes.Terran_Command_Center.ordinal())
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;
+				
+				if (building != null && building.getID() == UnitTypes.Terran_Command_Center.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Command_Center;
 		}
 		else if (unitType == UnitTypes.Terran_Bunker.ordinal() || unitType == UnitTypes.Terran_Academy.ordinal() || unitType == UnitTypes.Terran_Factory.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;
+				
 				if (building.getID() == UnitTypes.Terran_Barracks.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Barracks;
 		}
 		else if (unitType == UnitTypes.Terran_Starport.ordinal() || unitType == UnitTypes.Terran_Armory.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;
+				
 				if (building.getID() == UnitTypes.Terran_Factory.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Factory;
 		}
 		else if (unitType == UnitTypes.Terran_Science_Facility.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;
+				
 				if (building.getID() == UnitTypes.Terran_Starport.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Starport;
 		}
 
 		else if (unitType == UnitTypes.Terran_Marine.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;
+				
 				if (building.getID() == UnitTypes.Terran_Barracks.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Barracks;
 		}
 		else if (unitType == UnitTypes.Terran_Firebat.ordinal() || unitType == UnitTypes.Terran_Medic.ordinal())
 		{
-			for (Unit building1 : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
 			{
+				building1 = mBld.buildingsStack.get(i).unit;
+				
 				if (building1.getID() == UnitTypes.Terran_Barracks.ordinal())
 				{
-					for (Unit building2 : this.mBld.builtBuildings)	
+					for (int j = 0; j < mBld.completedBuildingsIndex; j++)
 					{
+						building2 = mBld.buildingsStack.get(j).unit;
+						
 						if (building2.getID() == UnitTypes.Terran_Academy.ordinal())						
 							return null;						
 					}
@@ -83,16 +110,22 @@ public class DescisionTree {
 		}
 		else if (unitType == UnitTypes.Terran_Ghost.ordinal())
 		{
-			for (Unit building1 : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
 			{
+				building1 = mBld.buildingsStack.get(i).unit;
+				
 				if (building1.getID() == UnitTypes.Terran_Barracks.ordinal())
 				{
-					for (Unit building2 : this.mBld.builtBuildings)	
+					for (int j = 0; j < mBld.completedBuildingsIndex; j++)
 					{
+						building2 = mBld.buildingsStack.get(j).unit;
+						
 						if (building2.getID() == UnitTypes.Terran_Academy.ordinal())	
 						{
-							for (Unit building3 : this.mBld.builtBuildings)	
+							for (int k = 0; k < mBld.completedBuildingsIndex; k++)
 							{
+								building3 = mBld.buildingsStack.get(k).unit;
+								
 								if (building3.getID() == UnitTypes.Terran_Science_Facility.ordinal())	
 								{
 									return null;
@@ -108,28 +141,38 @@ public class DescisionTree {
 		}
 		else if (unitType == UnitTypes.Terran_Vulture.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;		
 				if (building.getID() == UnitTypes.Terran_Factory.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Factory;
 		}
 
 		else if (unitType == UnitTypes.Terran_Wraith.ordinal())
 		{
-			for (Unit building : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
+			{
+				building = mBld.buildingsStack.get(i).unit;		
 				if (building.getID() == UnitTypes.Terran_Starport.ordinal())
 					return null;
+			}
 			return UnitTypes.Terran_Starport;
 		}
 
 		else if (unitType == UnitTypes.Terran_Siege_Tank_Tank_Mode.ordinal() || unitType == UnitTypes.Terran_Siege_Tank_Siege_Mode.ordinal())
 		{
-			for (Unit building1 : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
 			{
+				building1 = mBld.buildingsStack.get(i).unit;
+				
 				if (building1.getID() == UnitTypes.Terran_Factory.ordinal())
 				{
-					for (Unit building2 : this.mBld.builtBuildings)	
+					for (int j = 0; j < mBld.completedBuildingsIndex; j++)
 					{
+						building2 = mBld.buildingsStack.get(j).unit;
+						
 						if (building2.getID() == UnitTypes.Terran_Machine_Shop.ordinal())						
 							return null;						
 					}
@@ -140,12 +183,16 @@ public class DescisionTree {
 		}
 		else if (unitType == UnitTypes.Terran_Goliath.ordinal())
 		{
-			for (Unit building1 : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
 			{
+				building1 = mBld.buildingsStack.get(i).unit;
+				
 				if (building1.getID() == UnitTypes.Terran_Factory.ordinal())
 				{
-					for (Unit building2 : this.mBld.builtBuildings)	
+					for (int j = 0; j < mBld.completedBuildingsIndex; j++)
 					{
+						building2 = mBld.buildingsStack.get(j).unit;
+						
 						if (building2.getID() == UnitTypes.Terran_Armory.ordinal())						
 							return null;						
 					}
@@ -157,16 +204,22 @@ public class DescisionTree {
 
 		else if (unitType == UnitTypes.Terran_Science_Vessel.ordinal())
 		{
-			for (Unit building1 : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
 			{
+				building1 = mBld.buildingsStack.get(i).unit;
+				
 				if (building1.getID() == UnitTypes.Terran_Control_Tower.ordinal())
 				{
-					for (Unit building2 : this.mBld.builtBuildings)	
+					for (int j = 0; j < mBld.completedBuildingsIndex; j++)
 					{
+						building2 = mBld.buildingsStack.get(j).unit;
+						
 						if (building2.getID() == UnitTypes.Terran_Academy.ordinal())	
 						{
-							for (Unit building3 : this.mBld.builtBuildings)	
+							for (int k = 0; k < mBld.completedBuildingsIndex; k++)
 							{
+								building3 = mBld.buildingsStack.get(k).unit;
+								
 								if (building3.getID() == UnitTypes.Terran_Science_Facility.ordinal())	
 								{
 									return null;
@@ -183,12 +236,16 @@ public class DescisionTree {
 
 		else if (unitType == UnitTypes.Terran_Goliath.ordinal())
 		{
-			for (Unit building1 : this.mBld.builtBuildings)		
+			for (int i = 0; i < mBld.completedBuildingsIndex; i++)
 			{
+				building1 = mBld.buildingsStack.get(i).unit;
+				
 				if (building1.getID() == UnitTypes.Terran_Starport.ordinal())
 				{
-					for (Unit building2 : this.mBld.builtBuildings)	
+					for (int j = 0; j < mBld.completedBuildingsIndex; j++)
 					{
+						building2 = mBld.buildingsStack.get(j).unit;
+						
 						if (building2.getID() == UnitTypes.Terran_Control_Tower.ordinal())						
 							return null;						
 					}
